72cc648f001ea3a4e62b4f2f0012631df643cf63
Gsharp73 <tripathidhyanendra2004@gmail.com>
2025-06-04 16:36:21 +0530

    debugging

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const http = require('http');
 const { Server } = require('socket.io');
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
